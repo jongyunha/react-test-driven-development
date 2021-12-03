990c679114e764c8dfae110f3e8a3d86dd4a312f
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-03 20:31:07 +0900

    Dry refactoring

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -4,21 +4,28 @@ import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, value) => {
+  return wrapper.find(`[data-test='${value}']`);
+};
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 });
 
 test('renders increment button', () => {
-  const wrapper = shallow(<App />);
-  const button = wrapper.find("[data-test='increment-button']");
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
   expect(button.length).toBe(1);
 });
 
 test('renders counter display', () => {
-  const wrapper = shallow(<App />);
-  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+
   expect(counterDisplay.length).toBe(1);
 });
 
