7af949f0c0c423d8aa211319eeed9305e771be69
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-03 15:26:34 +0900

    First data-test Attibute Test

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -4,7 +4,11 @@ import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders without error', () => {});
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
 
 test('renders increment button', () => {});
 
