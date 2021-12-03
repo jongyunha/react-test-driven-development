540ca753a647e0e98393347ac32420ea8c466fc0
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-03 21:12:14 +0900

    Test Button Click

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div data-test="component-app">
-      <h1 data-test="counter-display">The counter is currently</h1>
+      <h1 data-test="counter-display">
+        The counter is currently&nbsp;
+        <span data-test="count">{count}</span>
+      </h1>
       <button data-test="increment-button">Increment</button>
     </div>
   );
