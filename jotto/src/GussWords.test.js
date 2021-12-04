7564da6a15ffa138f7955596f6a822e9acb0be15
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-04 13:23:38 +0900

    Write Test and BeforeEach for 'no words guessed'

--- jotto/src/GussWords.test.js
+++ jotto/src/GussWords.test.js
@@ -15,3 +15,21 @@ const setup = (props = {}) => {
 test('does not throw warning with expected props', () => {
   checkProps(GuessWords, defaultProps);
 });
+
+describe('if there are no words guessed', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWord: [] });
+  });
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+
+  test('renders instructions to guess a word', () => {
+    const instructions = findByTestAttr(wrapper, 'guess-instructions');
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+
+describe('if there are words guessed', () => {});
