3eeba6348695cdeab64b7cbbc588425e09542375
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-04 11:20:41 +0900

    Code the Congrats Component

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -1,7 +1,18 @@
 // receive the success state as a prop
 
 const Congrats = ({ success }) => {
-  return <div></div>;
+  return (
+    <>
+      {success && (
+        <div data-test="component-congrats">
+          <span data-test="congrats-message">
+            Congratulations! You guessed the word!
+          </span>
+        </div>
+      )}
+      {!success && <div data-test="component-congrats" />}
+    </>
+  );
 };
 
 export default Congrats;
